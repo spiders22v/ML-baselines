d68d0f34b43b317de0260cf25cbaf780b34236b4
spiders22v <spiders22v@gmail.com>
2024-12-12 14:48:18 +0900

    Update torch_gpu_cpu_comparison.py

--- util/torch_gpu_cpu_comparison.py
+++ util/torch_gpu_cpu_comparison.py
@@ -1,7 +1,8 @@
 # GPU, CPU 성능 비교
 # (1) NVIDIA AGX ORIN                GPU(5.06s) vs CPU(213.38s)   42.15배 차이
-# (2) AMD3965WX RTX3090              GPU(1.05s) vs CPU(  8.62s)    8.11배 차이 
+# (2) AMD 3965WX RTX3090             GPU(1.05s) vs CPU(  8.62s)    8.11배 차이 
 # (3) i9-13980HX RTX4080(Laptop)     GPU(1.25s) vs CPU( 20.12s)   16.12배 차이 
+# (4) AMD 5600 RTX3060               GPU(2.17s) vs CPU( 32.32s)   14.89배 차이 
 
 import torch
 import torch.nn as nn
